f9c3fed82d9d04d12150800e3974ae6b9641226d
Aimberê Galdino <biroska@gmail.com>
2021-09-18 14:48:27 -0300

    Filtrando e exibindo os registros das meals do arquivo de mock

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals/data/dummy_data.dart';
 import 'package:meals/models/category.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
@@ -7,12 +8,17 @@ class CategoriesMealsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final category = ModalRoute.of(context)?.settings?.arguments as Category;
 
+    final categoryMeals = DUMMY_MEALS.where( (meal) => meal.categories.contains( category.id )  ).toList();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(category.title),
       ),
-      body: Center(
-        child: Text("Receitas por categoria: ${category.id}"),
+      body: ListView.builder(
+        itemCount: categoryMeals.length,
+        itemBuilder: ( context, index ) {
+          return Text( categoryMeals[ index ].title );
+        }
       ),
     );
   }
