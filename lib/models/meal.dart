61efeadada66f511f9b5d80853456cc33fb1cf8d
Aimberê Galdino <biroska@gmail.com>
2021-09-18 14:37:27 -0300

    Incluindo o Meals data para mockar as receitas

---
+++ lib/models/meal.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/cupertino.dart';
+import 'package:meals/models/category.dart';
+
+class Meal {
+  final String id;
+  final String title;
+  final String imageUrl;
+  final int duration;
+  final bool isVegan;
+  final bool isGlutenFree;
+  final bool isLactoseFree;
+  final bool isVegetarian;
+
+  final Cost cost;
+  final Complexity complexity;
+
+  final List<String> categories;
+  final List<String> steps;
+  final List<String> ingredients;
+
+  const Meal(
+      {
+        required this.id,
+        required this.categories,
+        required this.title,
+        required this.cost,
+        required this.complexity,
+        required this.imageUrl,
+        required this.duration,
+        required this.ingredients,
+        required this.steps,
+        required this.isGlutenFree,
+        required this.isVegan,
+        required this.isVegetarian,
+        required this.isLactoseFree
+      });
+}
+
+enum Complexity {
+  Simple,
+  Medium,
+  Difficult
+}
+
+enum Cost {
+  Cheap,
+  Fair,
+  Expensive
+}
+
