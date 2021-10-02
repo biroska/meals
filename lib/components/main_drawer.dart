8acedcf73212a9bdae3161ab569ae96e403b2c2f
Aimberê Galdino <biroska@gmail.com>
2021-10-02 14:26:05 -0300

    Inclusao de Drawer (menu lateral)

---
+++ lib/components/main_drawer.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class MainDrawer extends StatelessWidget {
+
+  Widget _createItem(IconData icon, String label){
+
+    return ListTile(
+      leading: Icon(
+        icon, size: 26,
+      ),
+      title: Text( label,
+      style: const TextStyle(
+        fontFamily: 'RobotoCondensed',
+        fontSize: 24,
+        fontWeight: FontWeight.bold
+      ),),
+      onTap: (){},
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+          children:<Widget>[
+            Container(
+              height: 120,
+              width: double.infinity,
+              padding: const EdgeInsets.all( 20 ),
+              alignment: Alignment.bottomRight,
+              color: Theme.of(context).colorScheme.secondary,
+              child: Text('Vamos cozinhar?',
+              style: TextStyle(
+                fontWeight: FontWeight.w900,
+                fontSize: 30,
+                color: Theme.of(context).primaryColor
+              ),),
+            ),
+            const SizedBox(height: 20,),
+            _createItem( Icons.restaurant, 'Refeições' ),
+            _createItem( Icons.settings, 'configurações')
+          ]
+      ),
+    );
+  }
+}
--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals/components/main_drawer.dart';
 import 'package:meals/screens/categories_screen.dart';
 import 'package:meals/screens/favorite_screen.dart';
 
@@ -32,6 +33,7 @@ class _TabsScreenState extends State<TabsScreen> {
         title: Text(_titles[_selectedScreenIndex]),
       ),
       body: _screens[ _selectedScreenIndex ],
+      drawer: MainDrawer(),
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectScreen,
         backgroundColor: Theme.of(context).primaryColor,
