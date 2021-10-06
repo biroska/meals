10a40f1a6cd6fb9a8910fc260a353155adcb3f32
Aimberê Galdino <biroska@gmail.com>
2021-10-06 18:46:46 -0300

    Tela de configuracoes com SwitchListTile

---
+++ lib/models/settings.dart
@@ -0,0 +1,12 @@
+class Settings {
+  bool isVegan;
+  bool isGlutenFree;
+  bool isLactoseFree;
+  bool isVegetarian;
+
+  Settings(
+      {this.isVegan = false,
+      this.isGlutenFree = false,
+      this.isLactoseFree = false,
+      this.isVegetarian = false});
+}
